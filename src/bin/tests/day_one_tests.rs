02594c44974d3967e25f564d99a82f4ee76ba742
chuck <chuck-bates@pluralsight.com>
2020-12-02 22:00:44 -0700

    Refactor day one to have basic example tests

---
+++ src/bin/tests/day_one_tests.rs
@@ -0,0 +1,15 @@
+use super::*;
+
+#[test]
+fn test_part_one_example() {
+    let input = vec![1721,979,366,299,675,1456];
+    let expected = vec![1721,299];
+    assert_eq!(execute_part_one(input), expected);
+}
+
+#[test]
+fn test_part_two_example() {
+    let input = vec![1721,979,366,299,675,1456];
+    let expected = vec![979,366,675];
+    assert_eq!(execute_part_two(input), expected);
+}
