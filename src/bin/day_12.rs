ff5374a240c9977d3752990ac98be2ce04fc9862
chuck <chuck-bates@pluralsight.com>
2020-12-12 22:40:35 -0700

    day 12

--- src/bin/day_12.rs
+++ src/bin/day_12.rs
@@ -1,17 +1,214 @@
+use std::time::Instant;
+use std::convert::TryInto;
 mod input;
 
 fn main() {
+    let input_file_path = "src/bin/inputs/day_12_input.txt";
+    let now = Instant::now();
 
+    // Part One
+    let part_one_result = execute_part_one(input_file_path);
+    println!("({} seconds) distance: {}", now.elapsed().as_secs_f32(), part_one_result);
+
+    // Part Two
+    let part_two_result = execute_part_two(input_file_path);
+    println!("({} seconds) distance: {}", now.elapsed().as_secs_f32(), part_two_result);
 }
 
-fn execute_part_one(file_path: &str) -> usize {
-    // let lines = input::parse_strings(file_path);
-    unimplemented!();
+fn execute_part_one(file_path: &str) -> isize {
+    let lines = input::parse_strings(file_path);
+    let mut position = (0,0);
+    let mut direction = "E".to_string();
+    for line in lines {
+        let result = handle_instruction(line, direction.to_string(), position.clone());
+        position = (result.clone().0, result.clone().1);
+        direction = result.clone().2.to_string();
+    }
+
+    return position.0.abs() + position.1.abs();
 }
 
-fn execute_part_two(file_path: &str) -> usize {
-    // let lines = input::parse_strings(file_path);
-    unimplemented!();
+fn execute_part_two(file_path: &str) -> isize {
+    let lines = input::parse_strings(file_path);
+    let mut ship_position = (0,0);
+    let mut waypoint_position = (10,1);
+    for line in lines {
+        let result = handle_instruction_part_two(line, ship_position.clone(), waypoint_position.clone());
+        ship_position = (result.clone().0, result.clone().1);
+        waypoint_position = (result.clone().2, result.clone().3);
+    }
+
+    return ship_position.0.abs() + ship_position.1.abs();
+}
+
+fn handle_instruction_part_two(instruction: String, ship_position: (isize, isize), waypoint_position: (isize, isize)) -> (isize, isize, isize, isize) {
+    let command = instruction[..1].to_string();
+    let value = instruction[1..].parse::<isize>().unwrap();
+    let mut new_ship_position = (0,0);
+    let mut new_waypoint_position = (0,0);
+
+    if command == "N" {
+        new_ship_position = ship_position;
+        new_waypoint_position.0 = waypoint_position.0;
+        new_waypoint_position.1 += waypoint_position.1 + value;
+    }
+
+    if command == "S" {
+        new_ship_position = ship_position;
+        new_waypoint_position.0 = waypoint_position.0;
+        new_waypoint_position.1 += waypoint_position.1 - value;
+    }
+
+    if command == "E" {
+        new_ship_position = ship_position;
+        new_waypoint_position.0 += waypoint_position.0 + value;
+        new_waypoint_position.1 = waypoint_position.1;
+    }
+
+    if command == "W" {
+        new_ship_position = ship_position;
+        new_waypoint_position.0 += waypoint_position.0 - value;
+        new_waypoint_position.1 = waypoint_position.1;
+    }
+
+    if command == "F" {
+        let previous_x_diff = waypoint_position.0 - ship_position.0;
+        let previous_y_diff = waypoint_position.1 - ship_position.1;
+        new_ship_position.0 += ship_position.0 + previous_x_diff * value;
+        new_ship_position.1 += ship_position.1 + previous_y_diff * value;
+        new_waypoint_position.0 += new_ship_position.0 + previous_x_diff;
+        new_waypoint_position.1 += new_ship_position.1 + previous_y_diff;
+    } 
+
+    if command == "R" {
+        new_ship_position = ship_position;
+
+        if value == 90 {
+            let result = rotate_waypoint_right_90(ship_position, waypoint_position);
+            new_waypoint_position.0 += result.0;
+            new_waypoint_position.1 += result.1;
+        }
+        if value == 180 {
+            let result = rotate_waypoint_right_90(ship_position, rotate_waypoint_right_90(ship_position, waypoint_position));
+            new_waypoint_position.0 += result.0;
+            new_waypoint_position.1 += result.1;
+        }
+        if value == 270 {
+            let result = rotate_waypoint_right_90(ship_position, rotate_waypoint_right_90(ship_position, rotate_waypoint_right_90(ship_position, waypoint_position)));
+            new_waypoint_position.0 += result.0;
+            new_waypoint_position.1 += result.1;
+        }
+    }
+
+    if command == "L" {
+        new_ship_position = ship_position;
+
+        if value == 90 {
+            let result = rotate_waypoint_left_90(ship_position, waypoint_position);
+            new_waypoint_position.0 += result.0;
+            new_waypoint_position.1 += result.1;            
+        }
+        if value == 180 {
+            let result = rotate_waypoint_left_90(ship_position, rotate_waypoint_left_90(ship_position, waypoint_position));
+            new_waypoint_position.0 += result.0;
+            new_waypoint_position.1 += result.1;
+        }
+        if value == 270 {
+            let result = rotate_waypoint_left_90(ship_position, rotate_waypoint_left_90(ship_position, rotate_waypoint_left_90(ship_position, waypoint_position)));
+            new_waypoint_position.0 += result.0;
+            new_waypoint_position.1 += result.1;
+        }
+    }
+
+    return (new_ship_position.0, new_ship_position.1, new_waypoint_position.0, new_waypoint_position.1);
+}
+
+fn rotate_waypoint_left_90(ship_position: (isize, isize), waypoint_position: (isize, isize)) -> (isize, isize) {
+    let x_direction = {
+        if waypoint_position.0 - ship_position.0 < 0 { "W".to_string() } else { "E".to_string() }
+    };
+    let y_direction = {
+        if waypoint_position.1 - ship_position.1 < 0 { "S".to_string() } else { "N".to_string() }
+    };
+    let previous_x_diff = (waypoint_position.0 - ship_position.0).abs();
+    let previous_y_diff = (waypoint_position.1 - ship_position.1).abs();
+
+    let new_waypoint_x = {
+        if y_direction == "S" { ship_position.0 + previous_y_diff } else { ship_position.0 - previous_y_diff }
+    };
+    let new_waypoint_y = {
+        if x_direction == "E" { ship_position.1 + previous_x_diff } else { ship_position.1 - previous_x_diff }
+    };
+    return (new_waypoint_x, new_waypoint_y);
+}
+
+fn rotate_waypoint_right_90(ship_position: (isize, isize), waypoint_position: (isize, isize)) -> (isize, isize) {
+    let x_direction = {
+        if waypoint_position.0 - ship_position.0 < 0 { "W".to_string() } else { "E".to_string() }
+    };
+    let y_direction = {
+        if waypoint_position.1 - ship_position.1 < 0 { "S".to_string() } else { "N".to_string() }
+    };
+    let previous_x_diff = (waypoint_position.0 - ship_position.0).abs();
+    let previous_y_diff = (waypoint_position.1 - ship_position.1).abs();
+    let new_waypoint_x = {
+        if y_direction == "S" { ship_position.0 - previous_y_diff } else { ship_position.0 + previous_y_diff }
+    };
+    let new_waypoint_y = {
+        if x_direction == "E" { ship_position.1 - previous_x_diff } else { ship_position.1 + previous_x_diff }
+    };
+    return (new_waypoint_x, new_waypoint_y);
+}
+
+fn handle_instruction(instruction: String, direction: String, ship_position: (isize, isize)) -> (isize, isize, String) {
+    let command = instruction[..1].to_string();
+    let value = instruction[1..].parse::<isize>().unwrap();
+
+    if command == "F" {
+        let new_position = move_in_direction(direction.clone(), value, ship_position);
+        return (new_position.0, new_position.1, direction);
+    } 
+
+    let mut new_direction = direction.clone();
+    if command == "R" || command == "L" {
+        new_direction = rotate_direction(direction.clone(), command.clone(), value);
+    }
+    
+    let new_position = move_in_direction(command, value, ship_position);
+    return (new_position.0, new_position.1, new_direction);
+}
+
+fn move_in_direction(direction: String, value: isize, position: (isize, isize)) -> (isize, isize) {
+    let mut new_position = position.clone();
+    if direction == "E" || direction == "W" {
+        if direction == "E" {new_position.0 += value} else {new_position.0 -= value};
+    }
+
+    if direction == "N" || direction == "S" {
+        if direction == "N" {new_position.1 += value} else {new_position.1 -= value};
+    }
+
+    return new_position;
+}
+
+fn rotate_direction(direction: String, command: String, value: isize) -> String {
+    if value == 0 {
+        return direction;
+    }
+
+    let directions = vec!["N","E","S","W"];
+    let steps = if value == 90 {1} else if value == 180 {2} else if value == 270 {3} else {0};
+    let mut index: isize = directions.iter().position(|&s| s.to_string() == direction).unwrap().try_into().unwrap();
+    if command == "R" {
+        index += steps;
+        if index >= directions.len() as isize { index -= directions.len() as isize} else { index = index };
+    }
+    if command == "L" {
+        index -= steps;
+        if index < 0 { index += directions.len() as isize} else { index = index };
+    }
+
+    return directions[index as usize].to_string();
 }
 
 #[cfg(test)]
--- src/bin/tests/day_12_tests.rs
+++ src/bin/tests/day_12_tests.rs
@@ -1,17 +1,273 @@
 use super::*;
 
-#[test]
-fn when_executing_part_one() {
-    let input = "";
-    let expected = 0;
+// #[test]
+// fn when_executing_part_one() {
+//     let input = "";
+//     let expected = 0;
 
-    assert_eq!(execute_part_one(input), expected);
+//     assert_eq!(execute_part_one(input), expected);
+// }
+
+
+#[test]
+fn when_handling_an_instruction_and_it_is_a_forward_instruction() {
+    let instruction = "F2".to_string();
+    let direction = "E".to_string();
+    let position = (0,0);
+    let expected = (2,0,"E".to_string());
+
+    assert_eq!(handle_instruction(instruction, direction, position), expected);
 }
 
 #[test]
-fn when_executing_part_two() {
-    let input = "";
-    let expected = 0;
+fn when_handling_an_instruction_and_it_is_a_north_instruction() {
+    let instruction = "N2".to_string();
+    let direction = "E".to_string();
+    let position = (0,0);
+    let expected = (0,2,"E".to_string());
 
-    assert_eq!(execute_part_one(input), expected);
+    assert_eq!(handle_instruction(instruction, direction, position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_and_it_is_a_south_instruction() {
+    let instruction = "S2".to_string();
+    let direction = "E".to_string();
+    let position = (0,0);
+    let expected = (0,-2,"E".to_string());
+
+    assert_eq!(handle_instruction(instruction, direction, position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_and_it_is_a_west_instruction() {
+    let instruction = "W2".to_string();
+    let direction = "E".to_string();
+    let position = (0,0);
+    let expected = (-2,0,"E".to_string());
+
+    assert_eq!(handle_instruction(instruction, direction, position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_and_it_is_an_east_instruction() {
+    let instruction = "E2".to_string();
+    let direction = "E".to_string();
+    let position = (0,0);
+    let expected = (2,0,"E".to_string());
+
+    assert_eq!(handle_instruction(instruction, direction, position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_and_it_is_a_right_turn_instruction() {
+    let instruction = "R90".to_string();
+    let direction = "E".to_string();
+    let position = (0,0);
+    let expected = (0,0,"S".to_string());
+
+    assert_eq!(handle_instruction(instruction, direction, position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_and_it_is_a_left_turn_instruction() {
+    let instruction = "L270".to_string();
+    let direction = "E".to_string();
+    let position = (0,0);
+    let expected = (0,0,"S".to_string());
+
+    assert_eq!(handle_instruction(instruction, direction, position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_part_two_and_it_is_a_waypoint_north_instruction() {
+    let instruction = "N3".to_string();
+    let ship_position = (1,2);
+    let waypoint_position = (10,1);
+
+    let expected = (1,2,10,4);
+
+    assert_eq!(handle_instruction_part_two(instruction, ship_position, waypoint_position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_part_two_and_it_is_a_waypoint_south_instruction() {
+    let instruction = "S10".to_string();
+    let ship_position = (1,2);
+    let waypoint_position = (10,1);
+
+    let expected = (1,2,10,-9);
+
+    assert_eq!(handle_instruction_part_two(instruction, ship_position, waypoint_position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_part_two_and_it_is_a_waypoint_east_instruction() {
+    let instruction = "E7".to_string();
+    let ship_position = (1,2);
+    let waypoint_position = (10,1);
+
+    let expected = (1,2,17,1);
+
+    assert_eq!(handle_instruction_part_two(instruction, ship_position, waypoint_position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_part_two_and_it_is_a_waypoint_west_instruction() {
+    let instruction = "W13".to_string();
+    let ship_position = (1,2);
+    let waypoint_position = (10,1);
+
+    let expected = (1,2,-3,1);
+
+    assert_eq!(handle_instruction_part_two(instruction, ship_position, waypoint_position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_part_two_and_it_is_a_waypoint_forward_instruction() {
+    let instruction = "F10".to_string();
+    let ship_position = (0,0);
+    let waypoint_position = (10,1);
+
+    let expected = (100,10,110,11);
+
+    assert_eq!(handle_instruction_part_two(instruction, ship_position, waypoint_position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_part_two_and_it_is_a_waypoint_forward_instruction_not_at_origin() {
+    let instruction = "F7".to_string();
+    let ship_position = (100,10);
+    let waypoint_position = (110,14);
+
+    let expected = (170,38,180,42);
+
+    assert_eq!(handle_instruction_part_two(instruction, ship_position, waypoint_position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_part_two_and_it_is_a_waypoint_forward_instruction_not_at_origin_2() {
+    let instruction = "F11".to_string();
+    let ship_position = (170,38);
+    let waypoint_position = (174,28);
+
+    let expected = (214,-72,218,-82);
+
+    assert_eq!(handle_instruction_part_two(instruction, ship_position, waypoint_position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_part_two_and_it_is_a_waypoint_rotate_right_90_instruction() {
+    let instruction = "R90".to_string();
+    let ship_position = (170,38);
+    let waypoint_position = (180,42);
+
+    let expected = (170,38,174,28);
+
+    assert_eq!(handle_instruction_part_two(instruction, ship_position, waypoint_position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_part_two_and_it_is_a_waypoint_rotate_right_90_instruction_2() {
+    let instruction = "R90".to_string();
+    let ship_position = (0,0);
+    let waypoint_position = (5,-2);
+
+    let expected = (0,0,-2,-5);
+
+    assert_eq!(handle_instruction_part_two(instruction, ship_position, waypoint_position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_part_two_and_it_is_a_waypoint_rotate_right_90_instruction_3() {
+    let instruction = "R90".to_string();
+    let ship_position = (0,0);
+    let waypoint_position = (-5,2);
+
+    let expected = (0,0,2,5);
+
+    assert_eq!(handle_instruction_part_two(instruction, ship_position, waypoint_position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_part_two_and_it_is_a_waypoint_rotate_left_90_instruction() {
+    let instruction = "L90".to_string();
+    let ship_position = (0,0);
+    let waypoint_position = (5,2);
+
+    let expected = (0,0,-2,5);
+
+    assert_eq!(handle_instruction_part_two(instruction, ship_position, waypoint_position), expected);
+}
+#[test]
+fn when_handling_an_instruction_part_two_and_it_is_a_waypoint_rotate_left_90_instruction_2() {
+    let instruction = "L90".to_string();
+    let ship_position = (0,0);
+    let waypoint_position = (5,-2);
+
+    let expected = (0,0,2,5);
+
+    assert_eq!(handle_instruction_part_two(instruction, ship_position, waypoint_position), expected);
+}
+#[test]
+fn when_handling_an_instruction_part_two_and_it_is_a_waypoint_rotate_left_90_instruction_3() {
+    let instruction = "L90".to_string();
+    let ship_position = (0,0);
+    let waypoint_position = (-5,2);
+
+    let expected = (0,0,-2,-5);
+
+    assert_eq!(handle_instruction_part_two(instruction, ship_position, waypoint_position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_part_two_and_it_is_a_waypoint_rotate_right_180_instruction() {
+    let instruction = "R180".to_string();
+    let ship_position = (0,0);
+    let waypoint_position = (5,2);
+
+    let expected = (0,0,-5,-2);
+
+    assert_eq!(handle_instruction_part_two(instruction, ship_position, waypoint_position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_part_two_and_it_is_a_waypoint_rotate_right_270_instruction() {
+    let instruction = "R270".to_string();
+    let ship_position = (0,0);
+    let waypoint_position = (5,2);
+
+    let expected = (0,0,-2,5);
+
+    assert_eq!(handle_instruction_part_two(instruction, ship_position, waypoint_position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_part_two_and_it_is_a_waypoint_rotate_left_180_instruction() {
+    let instruction = "L180".to_string();
+    let ship_position = (0,0);
+    let waypoint_position = (5,2);
+
+    let expected = (0,0,-5,-2);
+
+    assert_eq!(handle_instruction_part_two(instruction, ship_position, waypoint_position), expected);
+}
+
+#[test]
+fn when_handling_an_instruction_part_two_and_it_is_a_waypoint_rotate_left_270_instruction() {
+    let instruction = "L270".to_string();
+    let ship_position = (0,0);
+    let waypoint_position = (5,2);
+
+    let expected = (0,0,2,-5);
+
+    assert_eq!(handle_instruction_part_two(instruction, ship_position, waypoint_position), expected);
+}
+
+#[test]
+fn when_executing_part_two_example() {
+    let input = "src/bin/tests/inputs/day_12/input_example.txt";
+    let expected = 286;
+
+    assert_eq!(execute_part_two(input), expected);
 }
