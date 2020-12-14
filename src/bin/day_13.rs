b1237c604f49319217ce1eb905f1358b606e6c81
chuck <chuck-bates@pluralsight.com>
2020-12-14 11:32:10 -0700

    day 13

--- src/bin/day_13.rs
+++ src/bin/day_13.rs
@@ -1,17 +1,74 @@
+use std::time::Instant;
+use std::collections::BTreeMap;
 mod input;
 
 fn main() {
+    let now = Instant::now();
 
+    // Part One
+    let input_file_path_part_one = "src/bin/inputs/day_13_input_part_one.txt";
+    let part_one_result = execute_part_one(input_file_path_part_one);
+    println!("({} seconds) product of bus ID and time to wait: {}", now.elapsed().as_secs_f32(), part_one_result);
+
+    // Part Two
+    let input_file_path_part_one = "src/bin/inputs/day_13_input_part_two.txt";
+    let part_two_result = execute_part_two(input_file_path_part_one);
+    println!("({} seconds) earliest timestamp: {}", now.elapsed().as_secs_f32(), part_two_result);
 }
 
 fn execute_part_one(file_path: &str) -> usize {
-    // let lines = input::parse_strings(file_path);
-    unimplemented!();
+    let lines = input::parse_strings(file_path);
+    let time: usize = lines[0].parse::<usize>().unwrap();
+    let mut next_bus_departures = vec![];
+    for interval in lines[1].split(',') {
+        if interval == "x" {
+            continue;
+        }
+        let interval = interval.parse::<usize>().unwrap();
+        next_bus_departures.push((get_bus_next_departure(time, interval), interval));
+    }
+    next_bus_departures.sort();
+    return (next_bus_departures[0].0 - time) * next_bus_departures[0].1; 
 }
 
 fn execute_part_two(file_path: &str) -> usize {
-    // let lines = input::parse_strings(file_path);
-    unimplemented!();
+    let lines = input::parse_strings(file_path);
+    let mut bus_map = BTreeMap::new();
+    let mut index = 0;
+    let mut largest_interval = 0;
+    for interval in lines[1].split(',') {
+        if interval != "x" {
+            let window = interval.parse::<usize>().unwrap();
+            bus_map.insert(index, window);
+            if window > largest_interval {
+                largest_interval = window;
+            }
+        }
+        index += 1;
+    }
+
+    let mut time = 0;
+    let mut multiplier = 1;
+    for (index, bus_interval) in bus_map {
+        while (time + index) % bus_interval != 0 {
+            time += multiplier;
+        }
+        multiplier *= bus_interval;
+    }    
+
+    return time;
+}
+
+fn get_bus_next_departure(time: usize, bus_interval: usize) -> usize {
+    let mut result = time;
+    loop {
+        if result % bus_interval == 0 {
+            break;
+        }
+        result += 1;
+    }
+
+    return result;
 }
 
 #[cfg(test)]
--- src/bin/tests/day_13_tests.rs
+++ src/bin/tests/day_13_tests.rs
@@ -1,17 +1,89 @@
 use super::*;
 
 #[test]
+fn when_getting_a_buses_next_departure_at_time_939_and_the_bus_interval_is_7() {
+    let time = 939;
+    let bus_interval = 7;
+    let expected = 945;
+
+    assert_eq!(get_bus_next_departure(time, bus_interval), expected);
+}
+
+#[test]
+fn when_getting_a_buses_next_departure_at_time_939_and_the_bus_interval_is_13() {
+    let time = 939;
+    let bus_interval = 13;
+    let expected = 949;
+
+    assert_eq!(get_bus_next_departure(time, bus_interval), expected);
+}
+
+#[test]
+fn when_getting_a_buses_next_departure_at_time_1068700_and_the_bus_interval_is_59() {
+    let mut time = 1068710;
+    let bus_interval = 59;
+    let mut expected = 1068726;
+
+    assert_eq!(get_bus_next_departure(time, bus_interval), expected);
+
+    time = expected + 1;
+    expected = 1068785;
+
+    assert_eq!(get_bus_next_departure(time, bus_interval), expected);
+}
+
+#[test]
 fn when_executing_part_one() {
-    let input = "";
-    let expected = 0;
+    let input = "src/bin/tests/inputs/day_13/input_example.txt";
+    let expected = 295;
 
     assert_eq!(execute_part_one(input), expected);
 }
 
 #[test]
-fn when_executing_part_two() {
-    let input = "";
-    let expected = 0;
+fn when_executing_part_two_example() {
+    let input = "src/bin/tests/inputs/day_13/input_example_part_two.txt";
+    let expected = 1068781;
 
-    assert_eq!(execute_part_one(input), expected);
+    assert_eq!(execute_part_two(input), expected);
+}
+
+#[test]
+fn when_executing_part_two_example_1() {
+    let input = "src/bin/tests/inputs/day_13/input_example_small_1.txt";
+    let expected = 3417;
+
+    assert_eq!(execute_part_two(input), expected);
+}
+
+#[test]
+fn when_executing_part_two_example_2() {
+    let input = "src/bin/tests/inputs/day_13/input_example_small_2.txt";
+    let expected = 754018;
+
+    assert_eq!(execute_part_two(input), expected);
+}
+
+#[test]
+fn when_executing_part_two_example_3() {
+    let input = "src/bin/tests/inputs/day_13/input_example_small_3.txt";
+    let expected = 779210;
+
+    assert_eq!(execute_part_two(input), expected);
+}
+
+#[test]
+fn when_executing_part_two_example_4() {
+    let input = "src/bin/tests/inputs/day_13/input_example_small_4.txt";
+    let expected = 1261476;
+
+    assert_eq!(execute_part_two(input), expected);
+}
+
+#[test]
+fn when_executing_part_two_example_5() {
+    let input = "src/bin/tests/inputs/day_13/input_example_small_5.txt";
+    let expected = 1202161486;
+
+    assert_eq!(execute_part_two(input), expected);
 }
