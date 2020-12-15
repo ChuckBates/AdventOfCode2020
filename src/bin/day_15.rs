0aef0eaa758dc523405feb58dad13a5cfb14ed70
chuck <chuck-bates@pluralsight.com>
2020-12-15 15:06:00 -0700

    day 15

--- src/bin/day_15.rs
+++ src/bin/day_15.rs
@@ -1,17 +1,43 @@
+use std::time::Instant;
 mod input;
 
 fn main() {
+    let input_file_path = "src/bin/inputs/day_15_input.txt";
+    let now = Instant::now();
+
+    // Part One
+    let part_one_result = execute(input_file_path, 2020);
+    println!("({} seconds) final number spoken: {}", now.elapsed().as_secs_f32(), part_one_result);
+
+    // Part Two
+    let part_two_result = execute(input_file_path, 30000000);
+    println!("({} seconds) final number spoken: {}", now.elapsed().as_secs_f32(), part_two_result);
 
 }
 
-fn execute_part_one(file_path: &str) -> usize {
-    // let lines = input::parse_strings(file_path);
-    unimplemented!();
-}
+fn execute(file_path: &str, limit: usize) -> usize {
+    let lines = input::parse_strings(file_path);
+    let mut spoken_numbers = vec![0; limit];
+    let mut index = 1;
+    let mut last_spoken = 0;
+    for start_number in lines[0].split(",") {
+        spoken_numbers[start_number.parse::<usize>().unwrap()] = index;
+        index += 1;
+    }
 
-fn execute_part_two(file_path: &str) -> usize {
-    // let lines = input::parse_strings(file_path);
-    unimplemented!();
+    while index < limit {
+        let mut next = 0;
+        if spoken_numbers[last_spoken] != 0 {
+            next = index - spoken_numbers[last_spoken];
+        }
+
+        spoken_numbers[last_spoken] = index;
+        last_spoken = next;
+
+        index += 1;
+    }
+
+    return last_spoken;
 }
 
 #[cfg(test)]
--- src/bin/tests/day_15_tests.rs
+++ src/bin/tests/day_15_tests.rs
@@ -1,17 +1,113 @@
 use super::*;
 
 #[test]
-fn when_executing_part_one() {
-    let input = "";
-    let expected = 0;
+fn when_executing_part_one_for_example_1() {
+    let input = "src/bin/tests/inputs/day_15/input_example_1.txt";
+    let expected = 436;
 
-    assert_eq!(execute_part_one(input), expected);
+    assert_eq!(execute(input, 2020), expected);
 }
 
 #[test]
-fn when_executing_part_two() {
-    let input = "";
-    let expected = 0;
+fn when_executing_part_one_for_example_2() {
+    let input = "src/bin/tests/inputs/day_15/input_example_2.txt";
+    let expected = 1;
 
-    assert_eq!(execute_part_one(input), expected);
+    assert_eq!(execute(input, 2020), expected);
+}
+
+#[test]
+fn when_executing_part_one_for_example_3() {
+    let input = "src/bin/tests/inputs/day_15/input_example_3.txt";
+    let expected = 10;
+
+    assert_eq!(execute(input, 2020), expected);
+}
+
+#[test]
+fn when_executing_part_one_for_example_4() {
+    let input = "src/bin/tests/inputs/day_15/input_example_4.txt";
+    let expected = 27;
+
+    assert_eq!(execute(input, 2020), expected);
+}
+
+#[test]
+fn when_executing_part_one_for_example_5() {
+    let input = "src/bin/tests/inputs/day_15/input_example_5.txt";
+    let expected = 78;
+
+    assert_eq!(execute(input, 2020), expected);
+}
+
+#[test]
+fn when_executing_part_one_for_example_6() {
+    let input = "src/bin/tests/inputs/day_15/input_example_6.txt";
+    let expected = 438;
+
+    assert_eq!(execute(input, 2020), expected);
+}
+
+#[test]
+fn when_executing_part_one_for_example_7() {
+    let input = "src/bin/tests/inputs/day_15/input_example_7.txt";
+    let expected = 1836;
+
+    assert_eq!(execute(input, 2020), expected);
+}
+
+#[test]
+fn when_executing_part_two_for_example_1() {
+    let input = "src/bin/tests/inputs/day_15/input_example_1.txt";
+    let expected = 175594;
+
+    assert_eq!(execute(input, 30000000), expected);
+}
+
+#[test]
+fn when_executing_part_two_for_example_2() {
+    let input = "src/bin/tests/inputs/day_15/input_example_2.txt";
+    let expected = 2578;
+
+    assert_eq!(execute(input, 30000000), expected);
+}
+
+#[test]
+fn when_executing_part_two_for_example_3() {
+    let input = "src/bin/tests/inputs/day_15/input_example_3.txt";
+    let expected = 3544142;
+
+    assert_eq!(execute(input, 30000000), expected);
+}
+
+#[test]
+fn when_executing_part_two_for_example_4() {
+    let input = "src/bin/tests/inputs/day_15/input_example_4.txt";
+    let expected = 261214;
+
+    assert_eq!(execute(input, 30000000), expected);
+}
+
+#[test]
+fn when_executing_part_two_for_example_5() {
+    let input = "src/bin/tests/inputs/day_15/input_example_5.txt";
+    let expected = 6895259;
+
+    assert_eq!(execute(input, 30000000), expected);
+}
+
+#[test]
+fn when_executing_part_two_for_example_6() {
+    let input = "src/bin/tests/inputs/day_15/input_example_6.txt";
+    let expected = 18;
+
+    assert_eq!(execute(input, 30000000), expected);
+}
+
+#[test]
+fn when_executing_part_two_for_example_7() {
+    let input = "src/bin/tests/inputs/day_15/input_example_7.txt";
+    let expected = 362;
+
+    assert_eq!(execute(input, 30000000), expected);
 }
