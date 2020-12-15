261787d15f0adcc4de0635bfe3634a58382b59e0
chuck <chuck-bates@pluralsight.com>
2020-12-14 23:14:17 -0700

    day 14

--- src/bin/day_14.rs
+++ src/bin/day_14.rs
@@ -1,17 +1,170 @@
+use std::time::Instant;
+use std::collections::HashMap;
+extern crate itertools;
+use itertools::Itertools;
 mod input;
 
 fn main() {
+    let input_file_path = "src/bin/inputs/day_14_input.txt";
+    let now = Instant::now();
 
+    // Part One
+    let part_one_result = execute_part_one(input_file_path);
+    println!("({} seconds) sum of non-zero values: {}", now.elapsed().as_secs_f32(), part_one_result);
+
+    // Part Two
+    let part_two_result = execute_part_two(input_file_path);
+    println!("({} seconds) sum of non-zero values: {}", now.elapsed().as_secs_f32(), part_two_result);
 }
 
 fn execute_part_one(file_path: &str) -> usize {
-    // let lines = input::parse_strings(file_path);
-    unimplemented!();
+    let lines = input::parse_strings(file_path);
+    let result = execute_blocks_part_one(lines);
+
+    let non_zero_values = result.values().filter(|&m| m > &0);
+    return non_zero_values.sum();
 }
 
 fn execute_part_two(file_path: &str) -> usize {
-    // let lines = input::parse_strings(file_path);
-    unimplemented!();
+    let lines = input::parse_strings(file_path);
+    let result = execute_blocks_part_two(lines);
+
+    let non_zero_values = result.values().filter(|&m| m > &0);
+    return non_zero_values.sum();
+}
+
+fn execute_blocks_part_one(lines: Vec<String>) -> HashMap<usize, usize> {
+    let mut result: HashMap<usize,usize> = HashMap::new();
+    let mut mask = "".to_string();
+    for line in lines {
+        if line.starts_with("mask") {
+            mask = line[7..].to_string();
+            continue;
+        }
+        let parts = line.split(" ").collect::<Vec<&str>>();
+        let value = parts[2].parse::<usize>().unwrap();
+        let address = parts[0][4..parts[0].len()-1].parse::<usize>().unwrap();
+        result.insert(address, mutate_value_with_mask(value, mask.clone()));
+    }
+
+    return result;
+}
+
+fn execute_blocks_part_two(lines: Vec<String>) -> HashMap<usize, usize> {
+    let mut result: HashMap<usize,usize> = HashMap::new();
+    let mut mask = "".to_string();
+    for line in lines {
+        if line.starts_with("mask") {
+            mask = line[7..].to_string();
+            continue;
+        }
+        let parts = line.split(" ").collect::<Vec<&str>>();
+        let value = parts[2].parse::<usize>().unwrap();
+        let address = parts[0][4..parts[0].len()-1].parse::<usize>().unwrap();
+        let permutations = mutate_address_with_mask(address, mask.clone());
+        for permutation in permutations {
+            result.insert(permutation, value);
+        }
+    }
+
+    return result;
+}
+
+fn mutate_address_with_mask(address: usize, mask: String) -> Vec<usize> {
+    let mut result: Vec<usize> = vec![];
+    let address_as_binary = format!("{:b}", address);
+    let difference = mask.len() - address_as_binary.len();
+    let mut rev_result = "".to_string();
+    let mut x_count = 0;
+    for index in (0..36).rev() {
+        let bit_mask = mask.chars().nth(index).unwrap();
+        let mut new_value = "0".to_string();
+
+        if index >= difference {
+            if bit_mask.to_string() == "1" {
+                new_value = "1".to_string();
+            } else if bit_mask.to_string() == "X" { 
+                new_value = "X".to_string();
+                x_count += 1;
+            } else {
+                new_value = address_as_binary.chars().nth(index - difference).unwrap().to_string();
+            }
+        } else {
+            if bit_mask.to_string() == "1" {
+                new_value = "1".to_string();
+            } else if bit_mask.to_string() == "X" {
+                new_value = "X".to_string();
+                x_count += 1;
+            }
+        }
+
+        rev_result = format!("{}{}", rev_result, new_value);
+    }
+
+    let address_as_string = rev_result.chars().rev().collect::<String>();
+    if x_count > 0 {
+        let address_permutations = fill_binary_combinations_into_result(get_all_binary_combinations(x_count), address_as_string);
+        for permutation in address_permutations {
+            result.push(usize::from_str_radix(&permutation, 2).unwrap());
+        }
+    } else {
+        let address_as_usize = usize::from_str_radix(&address_as_string, 2).unwrap();
+        result.push(address_as_usize);
+    }
+    return result;
+}
+
+fn get_all_binary_combinations(count: usize) -> Vec<Vec<usize>> {
+    let mut result = vec![];
+    for _ in 0..count {
+        result.push(0);
+        result.push(1);
+    }
+    let mut values = vec![];
+    for value in result.into_iter().combinations(count).unique() {
+        values.push(value);
+    }
+    return values;
+}
+
+fn fill_binary_combinations_into_result(combos: Vec<Vec<usize>>, result: String) -> Vec<String> {
+    let mut new_results = vec![];
+    for combo in combos {
+        let mut mutable_result = result.clone();
+        for value in combo {
+            mutable_result = mutable_result.clone().replacen("X", &value.to_string(), 1);
+        }
+        new_results.push(mutable_result.clone());
+    }
+    return new_results;
+}
+
+fn mutate_value_with_mask(value: usize, mask: String) -> usize {
+    let value_as_binary = format!("{:b}", value);
+    let difference = mask.len() - value_as_binary.len();
+    let mut rev_result = "".to_string();
+    for index in (0..36).rev() {
+        let bit_mask = mask.chars().nth(index).unwrap();
+        let mut new_value = "0".to_string();
+
+        if index >= difference {
+            if bit_mask.to_string() != "X" {
+                new_value = bit_mask.to_string();
+            } else {
+                new_value = value_as_binary.chars().nth(index - difference).unwrap().to_string();
+            }
+        } else {
+            if bit_mask.to_string() != "X" {
+                new_value = bit_mask.to_string();
+            }
+        }
+
+        rev_result = format!("{}{}", rev_result, new_value);
+    }
+
+    let result = rev_result.chars().rev().collect::<String>();
+
+    return usize::from_str_radix(&result, 2).unwrap();
 }
 
 #[cfg(test)]
--- src/bin/tests/day_14_tests.rs
+++ src/bin/tests/day_14_tests.rs
@@ -1,17 +1,235 @@
+use std::collections::HashMap;
 use super::*;
 
 #[test]
+fn when_mutating_a_value_with_a_mask_and_the_mask_is_all_blank() {
+    let value = 101;
+    let mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    let expected = 101;
+
+    assert_eq!(mutate_value_with_mask(value, mask.to_string()), expected);
+}
+
+#[test]
+fn when_mutating_a_value_with_a_mask_and_the_mask_is_not_blank_but_the_mask_lines_up() {
+    let value = 101;
+    let mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X";
+    let expected = 101;
+
+    assert_eq!(mutate_value_with_mask(value, mask.to_string()), expected);
+}
+
+#[test]
+fn when_mutating_a_value_with_a_mask_and_the_mask_is_not_blank_and_mutates_the_value() {
+    let value = 11;
+    let mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X";
+    let expected = 73;
+
+    assert_eq!(mutate_value_with_mask(value, mask.to_string()), expected);
+}
+
+#[test]
+fn when_mutating_a_value_with_a_mask_and_the_mask_is_not_blank_but_the_value_is_zero() {
+    let value = 0;
+    let mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X";
+    let expected = 64;
+
+    assert_eq!(mutate_value_with_mask(value, mask.to_string()), expected);
+}
+
+#[test]
+fn when_executing_blocks_part_one_and_there_is_only_one_memory_address_and_one_mask() {
+    let lines = vec![
+        "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X".to_string(),
+        "mem[8] = 11".to_string()
+    ];
+    let mut expected = HashMap::new();
+    expected.insert(8,73);
+
+    assert_eq!(execute_blocks_part_one(lines), expected);
+}
+
+#[test]
+fn when_executing_blocks_part_one_and_there_is_two_different_memory_addresses_and_one_mask() {
+    let lines = vec![
+        "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X".to_string(),
+        "mem[8] = 11".to_string(),
+        "mem[7] = 101".to_string()
+    ];
+    let mut expected = HashMap::new();
+    expected.insert(8,73);
+    expected.insert(7,101);
+
+    assert_eq!(execute_blocks_part_one(lines), expected);
+}
+
+#[test]
+fn when_executing_blocks_part_one_and_there_is_three_different_memory_addresses_and_two_masks() {
+    let lines = vec![
+        "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X".to_string(),
+        "mem[8] = 11".to_string(),
+        "mem[7] = 101".to_string(),
+        "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXX101".to_string(),
+        "mem[8] = 11".to_string(),
+    ];
+    let mut expected = HashMap::new();
+    expected.insert(8,77);
+    expected.insert(7,101);
+
+    let result = execute_blocks_part_one(lines);
+    assert_eq!(result.len(), 2);
+    assert_eq!(result, expected);
+}
+
+#[test]
 fn when_executing_part_one() {
-    let input = "";
-    let expected = 0;
+    let input = "src/bin/tests/inputs/day_14/input_example_part_one.txt";
+    let expected = 165;
 
     assert_eq!(execute_part_one(input), expected);
 }
 
 #[test]
-fn when_executing_part_two() {
-    let input = "";
-    let expected = 0;
+fn when_mutating_an_address_with_a_mask_and_the_mask_is_all_zeros() {
+    let address = 101;
+    let mask = "000000000000000000000000000000000000";
+    let expected = vec![101];
 
-    assert_eq!(execute_part_one(input), expected);
+    assert_eq!(mutate_address_with_mask(address, mask.to_string()), expected);
+}
+
+#[test]
+fn when_mutating_an_address_with_a_mask_and_the_mask_has_a_one() {
+    let address = 101;
+    let mask = "000000000000000000000000000000000010";
+    let expected = vec![103];
+
+    assert_eq!(mutate_address_with_mask(address, mask.to_string()), expected);
+}
+
+#[test]
+fn when_mutating_an_address_with_a_mask_and_the_mask_has_an_x() {
+    let address = 42;
+    let mask = "000000000000000000000000000000X1001X";
+    let expected = vec![27,26,58,59];
+
+    assert_eq!(mutate_address_with_mask(address, mask.to_string()), expected);
+}
+
+#[test]
+fn when_getting_binary_combinations_and_the_count_is_two() {
+    let count = 2;
+    let expected = vec![
+        vec![0,1],
+        vec![0,0],
+        vec![1,0],
+        vec![1,1]
+    ];
+
+    assert_eq!(get_all_binary_combinations(count), expected);
+}
+
+#[test]
+fn when_getting_binary_combinations_and_the_count_is_three() {
+    let count = 3;
+    let expected = vec![
+        [0, 1, 0], 
+        [0, 1, 1], 
+        [0, 0, 1], 
+        [0, 0, 0], 
+        [1, 0, 1], 
+        [1, 0, 0], 
+        [1, 1, 0], 
+        [1, 1, 1]
+    ];
+
+    assert_eq!(get_all_binary_combinations(count), expected);
+}
+
+#[test]
+fn when_filling_binary_combinations_into_a_result_and_there_are_four_combos() {
+    let combinations = vec![
+        vec![0,1],
+        vec![0,0],
+        vec![1,0],
+        vec![1,1]
+    ];
+    let result = "000000000000000000000000000000X1101X".to_string();
+    let expected = vec![
+        "000000000000000000000000000000011011".to_string(),
+        "000000000000000000000000000000011010".to_string(),
+        "000000000000000000000000000000111010".to_string(),
+        "000000000000000000000000000000111011".to_string()
+    ];
+
+    assert_eq!(fill_binary_combinations_into_result(combinations, result), expected);
+}
+
+#[test]
+fn when_filling_binary_combinations_into_a_result_and_there_are_eight_combos() {
+    let combinations = vec![
+        vec![0, 1, 0], 
+        vec![0, 1, 1], 
+        vec![0, 0, 1], 
+        vec![0, 0, 0], 
+        vec![1, 0, 1], 
+        vec![1, 0, 0], 
+        vec![1, 1, 0], 
+        vec![1, 1, 1]
+    ];
+    let result = "00000000000000000000000000000001X0XX".to_string();
+    let expected = vec![
+        "000000000000000000000000000000010010".to_string(),
+        "000000000000000000000000000000010011".to_string(),
+        "000000000000000000000000000000010001".to_string(),
+        "000000000000000000000000000000010000".to_string(),
+        "000000000000000000000000000000011001".to_string(),
+        "000000000000000000000000000000011000".to_string(),
+        "000000000000000000000000000000011010".to_string(),
+        "000000000000000000000000000000011011".to_string()
+    ];
+
+    assert_eq!(fill_binary_combinations_into_result(combinations, result), expected);
+}
+
+#[test]
+fn when_executing_blocks_part_two_with_four_permutations() {
+    let lines = vec![
+        "mask = 000000000000000000000000000000X1001X".to_string(),
+        "mem[42] = 100".to_string()
+    ];
+    let mut expected = HashMap::new();
+    expected.insert(27,100);
+    expected.insert(26,100);
+    expected.insert(58,100);
+    expected.insert(59,100);
+
+    assert_eq!(execute_blocks_part_two(lines), expected);
+}
+
+#[test]
+fn when_executing_blocks_part_two_with_eight_permutations() {
+    let lines = vec![
+        "mask = 00000000000000000000000000000000X0XX".to_string(),
+        "mem[26] = 1".to_string()
+    ];
+    let mut expected = HashMap::new();
+    expected.insert(16,1);
+    expected.insert(17,1);
+    expected.insert(18,1);
+    expected.insert(19,1);
+    expected.insert(24,1);
+    expected.insert(25,1);
+    expected.insert(26,1);
+    expected.insert(27,1);
+
+    assert_eq!(execute_blocks_part_two(lines), expected);
+}
+
+#[test]
+fn when_executing_part_two() {
+    let input = "src/bin/tests/inputs/day_14/input_example_part_two.txt";
+    let expected = 208;
+
+    assert_eq!(execute_part_two(input), expected);
 }
